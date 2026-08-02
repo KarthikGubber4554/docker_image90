5e37312250d1248883d997324c07b20519cde9b1
KarthikGubber4554 <gubberkarthik@gmail.com>
2026-08-02 10:46:14 +0530

    Update NammaDharawadaController.java

--- src/main/java/com/docker111/controller/NammaDharawadaController.java
+++ src/main/java/com/docker111/controller/NammaDharawadaController.java
@@ -3,15 +3,11 @@ package com.docker111.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class NammaDharawadaController {
 
-@RestController
-public class HelloBengaluruControllerMessage {
-	
-	@GetMapping("/message")
-	public String getMessage() {
-		return "HELLO DHARAWADA";
-	}
-
-}
+    @GetMapping("/message")
+    public String getMessage() {
+        return "HELLO DHARAWADA";
+    }
 }
