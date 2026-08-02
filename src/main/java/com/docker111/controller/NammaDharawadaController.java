73e2772a0480ec5bbbbae761164ff0fd66cab0fa
KarthikGubber4554 <gubberkarthik@gmail.com>
2026-08-02 10:54:04 +0530

    Update NammaDharawadaController.java

--- src/main/java/com/docker111/controller/NammaDharawadaController.java
+++ src/main/java/com/docker111/controller/NammaDharawadaController.java
@@ -8,6 +8,6 @@ public class NammaDharawadaController {
 
     @GetMapping("/message")
     public String getMessage() {
-        return "HELLO DAVANGERE";
+        return "HELLO Ranebennuru";
     }
 }
